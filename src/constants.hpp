c6aff9e571cf2f91d7e91d64921d7a825b25c43e
Tatsuro Iwanaka <136674384+tatsuro-iwanaka@users.noreply.github.com>
2026-09-03 13:22:24 +0900

    Create constants.hpp

---
+++ src/constants.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace paad::constants
+{
+	inline constexpr double AVOGADRO_CONSTANT = 6.02214076E23;
+	inline constexpr double MOLAR_GAS_CONSTANT = 8.31446261815324;
+	inline constexpr double PLANCK_CONSTANT = 6.62607015E-34;
+	inline constexpr double SPEED_OF_LIGHT = 2.99792458E8;
+	inline constexpr double BOLTZMANN_CONSTANT = 1.380649E-23;
+}
