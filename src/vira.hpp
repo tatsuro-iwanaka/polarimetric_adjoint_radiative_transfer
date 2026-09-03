c25dbe3e33a3b68f291d1c80e66c1c3bd81cb85d
Tatsuro Iwanaka <136674384+tatsuro-iwanaka@users.noreply.github.com>
2026-09-03 13:00:19 +0900

    Create vira.hpp

---
+++ src/vira.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+
+namespace paad::atmosphere::vira
+{
+	extern const std::vector<std::vector<double>> equator;
+	extern const std::vector<std::vector<double>> latitude_45;
+	extern const std::vector<std::vector<double>> latitude_60;
+}
