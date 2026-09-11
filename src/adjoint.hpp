5de243a2a6214b6f6bbfac5c7f853ec5ad429c95
Tatsuro Iwanaka <136674384+tatsuro-iwanaka@users.noreply.github.com>
2026-09-11 18:31:36 +0900

    Update adjoint.hpp

--- src/adjoint.hpp
+++ src/adjoint.hpp
@@ -28,23 +28,15 @@ RadiativeLayer doubleLayer_adjoint(const RadiativeLayer& layer, const geometry::
 std::vector<RadiativeLayer> addLayer_adjoint(const RadiativeLayer& layer_bottom, const RadiativeLayer& layer_top, const geometry::Geometry& geometry, const RadiativeLayer& adj_result, int n_parallel_fourier);
 OpticalSensitivity computeInitializationSensitivities(const RadiativeLayer& adj_layer, const RadiativeLayer& fwd_matrix_layer, double single_scattering_albedo, double planck_function, const geometry::Geometry& geo, int n_theta);
 
-struct InternalRadianceAdjointResult {
-    RadiativeLayer adj_layer_top;
-    RadiativeLayer adj_layer_bottom;
-    Eigen::VectorXd adj_I_minus_k_vec;
-    Eigen::MatrixXd adj_I_minus_k_mat;
+struct InternalRadianceAdjointResult
+{
+	RadiativeLayer adj_layer_top;
+	RadiativeLayer adj_layer_bottom;
+	Eigen::VectorXd adj_I_minus_k_vec;
+	Eigen::MatrixXd adj_I_minus_k_mat;
 };
 
-InternalRadianceAdjointResult computeInternalRadianceVector_adjoint(
-    const RadiativeLayer& layer_top, const RadiativeLayer& layer_bottom,
-    const Eigen::VectorXd& I_minus_k, const Eigen::VectorXd& I_minus_k_1,
-    const Eigen::VectorXd& adj_I_plus_k_1, const Eigen::VectorXd& adj_I_minus_k_1,
-    const geometry::Geometry& geo, int n_stokes);
-
-InternalRadianceAdjointResult computeInternalRadianceMatrix_adjoint(
-    const RadiativeLayer& layer_top, const RadiativeLayer& layer_bottom,
-    const Eigen::MatrixXd& I_minus_k, const Eigen::MatrixXd& I_minus_k_1,
-    const Eigen::MatrixXd& adj_I_plus_k_1, const Eigen::MatrixXd& adj_I_minus_k_1,
-    const geometry::Geometry& geo, int m, int n_stokes);
+InternalRadianceAdjointResult computeInternalRadianceVector_adjoint(const RadiativeLayer& layer_top, const RadiativeLayer& layer_bottom, const Eigen::VectorXd& I_minus_k, const Eigen::VectorXd& I_minus_k_1, const Eigen::VectorXd& adj_I_plus_k_1, const Eigen::VectorXd& adj_I_minus_k_1, const geometry::Geometry& geo, int n_stokes);
+InternalRadianceAdjointResult computeInternalRadianceMatrix_adjoint(const RadiativeLayer& layer_top, const RadiativeLayer& layer_bottom, const Eigen::MatrixXd& I_minus_k, const Eigen::MatrixXd& I_minus_k_1, const Eigen::MatrixXd& adj_I_plus_k_1, const Eigen::MatrixXd& adj_I_minus_k_1, const geometry::Geometry& geo, int m, int n_stokes);
 	
 }
